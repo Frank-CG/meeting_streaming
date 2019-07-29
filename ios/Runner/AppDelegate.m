8229e903c2c77a5bb173b42e93bc7a065bcbb7a4
frankg <frankg@sliq.com>
2019-07-29 13:20:44 -0400

    ss

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [FIRApp configure];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
